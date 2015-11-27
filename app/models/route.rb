f39eb6e96c925cab76c64f5b64724b52526c7a7d
Thiago Fantin <thiago_fantin@hotmail.com>
2015-11-26 23:12:08 -0200

    Update

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ActiveRecord::Base
   belongs_to :linha
   has_many :ruas
+  validates :linha_id, :presence => true
 end
--- app/models/rua.rb
+++ app/models/rua.rb
@@ -1,3 +1,5 @@
 class Rua < ActiveRecord::Base
   belongs_to :route
+  validates :nome, :presence => true
+  validates :route_id, :presence => true
 end
